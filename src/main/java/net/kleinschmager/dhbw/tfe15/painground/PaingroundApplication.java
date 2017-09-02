e79ffc7085c905ef4a0043fee3442e3b6fa70fe6
Robert Kleinschmager <robert@kleinschmager.net>
2017-09-02 16:21:27 +0200

    commit the pure sceleton from start.spring.io

---
+++ src/main/java/net/kleinschmager/dhbw/tfe15/painground/PaingroundApplication.java
@@ -0,0 +1,12 @@
+package net.kleinschmager.dhbw.tfe15.painground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaingroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaingroundApplication.class, args);
+	}
+}
---
+++ src/test/java/net/kleinschmager/dhbw/tfe15/painground/PaingroundApplicationTests.java
@@ -0,0 +1,16 @@
+package net.kleinschmager.dhbw.tfe15.painground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaingroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
