1ca5748eccbdc65116e4a250a69d036322c49df1
Robert Kleinschmager <robert.kleinschmager@actico.com>
2017-11-15 07:07:43 +0100

    begin to rework ui tests
    * use page object pattern
    * try to use more features from fluentlenium

--- src/test/java/net/kleinschmager/dhbw/tfe15/painground/PaingroundApplicationTests.java
+++ src/test/java/net/kleinschmager/dhbw/tfe15/painground/PaingroundApplicationTests.java
@@ -40,90 +40,11 @@ import net.kleinschmager.dhbw.tfe15.painground.persistence.repository.MemberProf
  *
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@FluentConfiguration(screenshotMode = TriggerMode.AUTOMATIC_ON_FAIL, screenshotPath = "target/ui-test-failures/")
-public class PaingroundApplicationTests extends FluentTest {
-
-	private static final MemberProfile TEST_ITEM_1 = new MemberProfile("testId1", "test Surname 1");
-
-	private String WAIT_FOR_VAADIN_SCRIPT = "if (window.vaadin == null) {" + "  return true;" + "}"
-			+ "var clients = window.vaadin.clients;" + "if (clients) {" + "  for (var client in clients) {"
-			+ "    if (clients[client].isActive()) {" + "      return false;" + "    }" + "  }" + "  return true;"
-			+ "} else {" +
-			// A Vaadin connector was found so this is most likely a Vaadin
-			// application. Keep waiting.
-			"  return false;" + "}";
-
-	@LocalServerPort
-	int randomPort;
-
-	@Autowired
-	MemberProfileRepository repository;
-
-	// private PhantomJSDriver webDriver = new PhantomJSDriver();
-	private ChromeDriver webDriver;
-
-	@Override
-	public WebDriver newWebDriver() {
-
-		final ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--disable-gpu");
-
-		final DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setJavascriptEnabled(true);
-		dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-
-		webDriver = new ChromeDriver(dc);
-		webDriver.manage().window().setSize(new Dimension(1280,1024));
-		return webDriver;
-	}
-
-	private String getUrlOfEmbeddedServer() {
-		return "http://localhost:" + randomPort;
-	}
-
-	@BeforeClass
-	public static void setupClass() {
-		ChromeDriverManager.getInstance().setup();
-	}
-
-	@Before
-	public void setUp() {
-		repository.deleteAll();
-		repository.save(Arrays.asList(TEST_ITEM_1));
-		repository.flush();
-	}
+@SpringBootTest
+public class PaingroundApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
-
-	@Test
-	public void loadPageAndMakeScreenshot() throws IOException {
-		goTo(getUrlOfEmbeddedServer());
-		await().atMost(10, TimeUnit.SECONDS).until($(".v-app-loading")).not().present();
-		// await().atMost(60,
-		// TimeUnit.SECONDS).until($(".v-loading-indicator")).not().present();
-		waitForVaadin();
-
-		File screenshot = webDriver.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshot, new File("target/screenshot_testcase_loadPageAndMakeScreenshot.png"));
-	}
-
-	/**
-	 * Block until Vaadin reports it has finished processing server messages.
-	 */
-	private void waitForVaadin() {
-
-		long timeoutTime = System.currentTimeMillis() + 20000;
-		Boolean finished = false;
-		while (System.currentTimeMillis() < timeoutTime && !finished) {
-			finished = (Boolean) ((JavascriptExecutor) webDriver).executeScript(WAIT_FOR_VAADIN_SCRIPT);
-			if (finished == null) {
-				finished = false;
-			}
-		}
 	}
 
 }
